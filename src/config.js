b145d1859ab7000bfcb65d5f0cb5436c93774457
Timurbl <32030037+Timurbl@users.noreply.github.com>
2018-08-25 19:58:52 +0300

    Готово

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  TOKEN: '475281982:AAG8Dv9Q7y7oWpbFHD6WkKHnYSFiN7m8pt8',
+  DB_URL: 'mongodb://localhost/testcinema'
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  telegramId: {
+    type: Number,
+    required: true
+  },
+  films: {
+    type: [String],
+    default: []
+  }
+},
+    {
+        usePushEach: true
+    });
+
+
+mongoose.model('users', UserSchema);
